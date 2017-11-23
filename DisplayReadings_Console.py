333b5b684b7564eb0c264e3e0143c12b53361108
Rico Castelo <ricorx7@gmail.com>
2017-11-22 21:38:09 -0800

    Update viewing DisplayReading_Console

--- DisplayReadings_Console.py
+++ DisplayReadings_Console.py
@@ -11,9 +11,11 @@ ipFrontRobot = 'RYC-Front'			# IP address for the front robot IP addresses
 ipRearRobot = 'RYC-Rear'			# IP address for the front robot IP addresses
 transferPort = 99					# Port number used when receiving updates from robots
 
-TCP_IP = ipFrontRobot
+#TCP_IP = ipFrontRobot
+TCP_IP = '0.0.0.0'
 TCP_PORT = transferPort
 
+"""
 data = ""
 try:
     sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -25,3 +27,24 @@ try:
     sender.close()
 except Exception as ex:
     print "Error connecting", ex
+"""
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+while True:
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+
+    data = conn.recv(BUFFER_SIZE)
+    if not data:
+        print "NO MORE DATA"
+        break
+    #print "received data:", data
+    items = data.split('|')
+    for item in items:
+        print item
+    print "-----------------------------------------"
+
+    #conn.send(data)  # echo
+conn.close()
